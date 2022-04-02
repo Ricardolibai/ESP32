533cf3718f4a3493a9de77ded63cd7036533bedb
crystal <2391434441@qq.com>
2022-04-02 13:41:14 +0800

    master

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+
+#include "Wire.h"
+#include <Adafruit_GFX.h>    
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Arduino.h>
+
+int pin = 2;
+int freq=5000 ;
+int channel = 0;
+int resolution = 9;
+
+
+
+void setup()
+{
+  ledcSetup(channel , freq , resolution);
+  ledcAttachPin(pin , channel);
+}
+
+void loop()
+{
+  for(int dutycycle = 0 ; dutycycle<= 2048 ; dutycycle ++)
+  {
+    ledcWrite(channel , dutycycle);
+    delay(15);
+  }
+    for(int dutycycle = 2048 ; dutycycle>=0 ; dutycycle --)
+  {
+    ledcWrite(channel , dutycycle);
+    delay(15);
+  }
+}
